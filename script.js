4646b7c9934effc7bc6206d3da6769fae1cbc4ee
nusingh <nusingh@cisco.com>
2025-06-23 23:48:51 +0530

    bug fixes

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 
 let expression = [];
 let num="";
+let equals=false;
 let table =  document.querySelector("table");
 let input = document.querySelector(".input");
 table.addEventListener("click",function(e){
     let value = e.target.innerText;
     let classValue = e.target.classList.value;
     if(classValue.includes("digit")){
-        // to perform after equalto behaviour
-        if(expression[0]=="equals"){
+        // to perform after equal to behaviour
+        if(equals){
             input.innerHTML="<span>.</span>";
-            expression = [];
             num = "";
+            equals=false;
         }
         // to prevent the decimal from inserting more than one time in a single digit
-        if(classValue.includes("decimal")){
-            if((num.match(/\./g) || []).length == 0){
-                num = num.concat(value);
-                input.innerHTML=input.innerHTML.concat(value);
-            }
+        if(classValue.includes("decimal") && num.includes(".")){
+            
         }
         else{
             num = num.concat(value);
@@ -27,7 +25,7 @@ table.addEventListener("click",function(e){
         input.scrollLeft = input.scrollWidth;
     }
     else if(classValue.includes("operator") && num!=""){
-        if(expression[0]=="equals") expression.shift(); // to perform operation directly on previous result
+        equals=false;
         expression.push(num);
         expression.push(value);
         input.innerHTML=input.innerHTML.concat(value);
@@ -37,8 +35,9 @@ table.addEventListener("click",function(e){
         expression.push(num);
         let ans = eval(expression.join(""));
         input.innerHTML=ans;
-        num="".concat(ans); // to perform operation directly on previous result
-        expression = ["equals"]; // to keep track of expression after equals
+        num=String(ans); // to perform operation directly on previous result
+        expression=[];
+        equals=true; // to keep track of expression after equals
     }
     else if(classValue.includes("clear")){
         expression = [];
